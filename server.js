457b6fbac2b415fef252f0f8e60c7867302e27af
brandonkhoza <8brandonkhoza@gmail.com>
2023-11-01 00:34:37 +0200

    configuring views with html and redirects

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
 const http = require('http');
-
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-
-
-
     console.log(`Getting the url : ${req.url} getting the method used : ${req.method}`);
 
     //setting what type of data is sent  back to the browser
-    
+
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
-    //content sent to the browser
-    res.write('<p>This was sent from a server</p>');
+    //path we want to read from
+    let path = './views/'
+    switch (req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200;
+            break;
 
-    //ending the response of data sent tp the browser
-    res.end();
+        case '/aboutus':
+            res.setHeader('Location', '/about');
+            res.statusCode = 301;
+            break;
+        default:
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
+
+
+    fs.readFile(path, (err, data) => {
+        if (err) {
+            console.log(err);
+            //ending the response of data sent to the browser
+            res.end()
+        } else {
+            //content sent to the browser if wriritng multiple things
+            // res.write(data);
+
+            //ending the response of data sent to the browser
+            res.end(data);
+        }
+    });
+
 
 });
-
 
 server.listen(3000, 'localhost', () => {
     console.log('Listening on port 3000');
